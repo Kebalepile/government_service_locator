e1e9167a234fa520cdc7f21bd10d627ec84e145b
Kebalepile <pimpabutterfly86@gmail.com>
2023-06-22 12:39:52 +0200

    index component welcome html text content"

--- src/components/pages/Index.jsx
+++ src/components/pages/Index.jsx
@@ -4,9 +4,31 @@ export default function Index() {
   return (
     <>
       <section id="homepage" className="page">
-        <h4>greeting message here</h4>
-        <h5>explain what the web app does here</h5>
-        <p>Call to action message.</p>
+        <h4>Welcome to the Government Service Locator (GLS)</h4>
+        <br />
+        <h4>
+          {" "}
+          Our app is designed to help you easily find and locate government
+          services in South Africa.
+        </h4>
+        <br />
+        <p>
+          <strong>How is this services provided ?</strong>
+         
+        </p>
+        <br />
+        <p>
+          We scrape data from official government websites such as departments,
+          agencies and public services to provide you with up-to-date
+          information on contact details, operating hours and services offered.
+          Currently, our app provides information on SAPS, clinics and community
+          health centers.{" "}
+        </p>
+        <br />
+        <h5>
+          We encourage you to try our app and discover how easy it is to find
+          the government services you need. Give it a try today!
+        </h5>
       </section>
     </>
   );
