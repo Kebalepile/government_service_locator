04a4cb77267ff624da693796ada04d4a3a7ce1d4
Kebalepile <pimpabutterfly86@gmail.com>
2023-06-22 13:49:05 +0200

    set search by dialog.

--- src/components/pages/PrimaryHealthFacilites.jsx
+++ src/components/pages/PrimaryHealthFacilites.jsx
@@ -1,28 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { BsArrowDown } from "react-icons/bs";
 import { BiMapPin } from "react-icons/bi";
 export default function PrimaryHealthFacility() {
+  const [showDialog, setShowDialog] = useState(false);
+
+  const handleOpenDialog = () => {
+    setShowDialog(true);
+  };
+
+  const handleCloseDialog = () => {
+    setShowDialog(false);
+  };
+  const loadMap = () => {
+    console.log("map.");
+  };
+  const searchBy = () => {};
+
   return (
-    <div className="page" id="healthcarepage">
-      <h4>Health Care:</h4>
-      <p>Search for facility by:</p>
-      <ol>
-        <li>Province</li>
-        <li>District</li>
-        <li>Municipality</li>
-      </ol>
-      <p><strong>or</strong></p>
-      <p>Use the Map</p>
-      <br />
-      <BsArrowDown id="arrow" />
-      <br/>
-      <section id="choice">
-        <button>search</button>
-        <button>
-          <BiMapPin />
-          load map
-        </button>
-      </section>
-    </div>
+    <>
+      <div className="page" id="healthcarepage">
+        <h4>Health Care:</h4>
+        <p>Search for facility by:</p>
+        <ol>
+          <li>Province</li>
+          <li>District</li>
+          <li>Municipality</li>
+        </ol>
+        <p>
+          <strong>or</strong>
+        </p>
+        <p>Use the Map</p>
+        <br />
+        <BsArrowDown id="arrow" />
+        <br />
+        <section id="choice">
+          <button onClick={handleOpenDialog}>search</button>
+          <button onClick={loadMap}>
+            <BiMapPin />
+            load map
+          </button>
+        </section>
+      </div>
+      <dialog open={showDialog} className="dialog-centered">
+        <p>Hello from dialog.</p>
+        <button onClick={handleCloseDialog}>close</button>
+      </dialog>
+    </>
   );
 }
