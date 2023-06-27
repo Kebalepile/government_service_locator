bc3f08d47ec61c7f1e519a7072cb29e4cbc403a2
Kebalepile <pimpabutterfly86@gmail.com>
2023-06-27 11:35:57 +0200

    service worker setup

--- src/main.jsx
+++ src/main.jsx
@@ -4,18 +4,18 @@ import HealthFacilitiesProvider from "./contexts/clinics/state";
 import ProtectionServicesProvider from "./contexts/saps/state";
 import App from "./App";
 
-// if ("serviceWorker" in navigator) {
-//   window.addEventListener("load", () => {
-//     navigator.serviceWorker
-//       .register("./src/sw.js")
-//       .then((registration) => {
-//         console.log("service worker registered as: ", registration.scope);
-//       })
-//       .catch((error) =>
-//         console.error("service worker registration: ", error)
-//       );
-//   });
-// }
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./src/serviceWorker.js")
+      .then((registration) => {
+        console.log("service worker registered as: ", registration.scope);
+      })
+      .catch((error) =>
+        console.error("service worker registration: ", error)
+      );
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
